f76717e3ae8e0fbec6948710323d8045c43840a0
oluwatoyinbello <teewhybells@gmail.com>
2023-02-20 14:13:10 +0100

    js-basics

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ function multiply(number1, number2) {
 }
 console.log(multiply(235,25)) 
 
-
-let firstname = 'Oluwatoyin';
-let lastname = 'Bello';
+// variables about me
+let firstname = ' Oluwatoyin ';
+let lastname = ' Bello ';
 let stuterntrack  = 'Frontend development';
-let skill     = 'Git, Html, css';
-let favoritecolour = 'Purple';
-const countryIndependence ='1960';
+let skills     = ' Git, Html and css ';
+let favoritecolour = ' Purple ';
+const countryIndependence =' 1960 ';
 
 // concatenating the above variables
+let myinfo = 
+' I am ' + 
+firstname +
+lastname +
+' a trainee of ' +
+stuterntrack +
+' at stutern ' + '.' +
+' My skills are ' +
+skills +
+' My favorite colour is ' +
+favoritecolour + ' . '
+' Nigeria my country gained her Independence in year ' +
+countryIndependence + ' . '
+console.log(myinfo)
